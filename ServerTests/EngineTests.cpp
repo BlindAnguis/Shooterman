b552be653a1f9746d81664e1f01b9c51a00765c1
Johan Jernberg <johan.jernberg@tieto.com>
2019-06-24 14:40:02 +0200

    Add test base
    Adding test base for doing unit test with Google Test

---
+++ ServerTests/EngineTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+TEST(EngineTests, test1) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(EngineTests, test2) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(EngineTests, test3) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
---
+++ ServerTests/EntityCreatorTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+TEST(EntityCreatorTests, createMage) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(EntityCreatorTests, createKnight) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(EntityCreatorTests, createSpearman) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
